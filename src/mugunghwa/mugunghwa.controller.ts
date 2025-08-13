347a1bba7582547c678d0b70baa30f2d7552f03b
Jadest <Jadest13x03@gmail.com>
2025-08-13 23:59:02 +0900

    getTotalFlowerCount 전체 공개로 변경

--- src/mugunghwa/mugunghwa.controller.ts
+++ src/mugunghwa/mugunghwa.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, Get } from '@nestjs/common';
 import { MugunghwaService } from './mugunghwa.service';
 import { PlantFlowerDto } from './dto/plant-flower.dto';
 import { GetUser } from 'src/common/decorators/get-user.decorator';
+import { Public } from 'src/common/decorators/public.decorator';
 
 @Controller('mugunghwa')
 export class MugunghwaController {
@@ -12,6 +13,7 @@ export class MugunghwaController {
     return this.mugunghwaService.plantFlower(user.email, plantFlowerDto);
   }
 
+  @Public()
   @Get('getTotalFlowerCount')
   getTotalFlowerCount() {
     return this.mugunghwaService.getTotalFlowerCount();
