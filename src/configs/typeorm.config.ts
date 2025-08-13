8b6ca65f60c2fc0dd019cffe172e179064c085d8
Jadest <Jadest13x03@gmail.com>
2025-08-13 23:26:13 +0900

    env 테스트

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,6 +4,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_SCHEMA);
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mongodb',
   url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_SCHEMA}`,
